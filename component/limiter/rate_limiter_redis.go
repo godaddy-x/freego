b285881662474f56d60fd7ace5fcd11fcc2e7116
godaddy-x <shadowsick@sina.com>
2022-08-20 14:12:32 +0800

    modify redis ratelimiter lua script

--- component/limiter/rate_limiter_redis.go
+++ component/limiter/rate_limiter_redis.go
@@ -11,52 +11,37 @@ const (
 )
 
 var limiterScript = redis.NewScript(1, `
-	-- LUA脚本会以单线程执行,不会有并发问题，一个脚本中的执行过程中如果报错，那么已执行的操作不会回滚
-	-- KEYS和ARGV是外部传入进来需要操作的redis数据库中的key,下标从1开始
-	-- 参数结构: KEYS = [限流的key]   ARGV = [最大令牌数, 每秒生成的令牌数, 本次请求的毫秒数]
-
-	local info = redis.pcall('HMGET', KEYS[1], 'last_time', 'stored_token_nums')
-	local last_time = info[1] --最后一次通过限流的时间
-	local stored_token_nums = tonumber(info[2]) -- 剩余的令牌数量
-	local max_token = tonumber(ARGV[1])
-	local token_rate = tonumber(ARGV[2])
-	local current_time = tonumber(ARGV[3])
-	local past_time = 0
-	local rateOfperMills = token_rate/1000 -- 每毫秒生产令牌速率
-	
-	if stored_token_nums == nil then
-		-- 第一次请求或者键已经过期
-		stored_token_nums = max_token --令牌恢复至最大数量
-		last_time = current_time --记录请求时间
+	-- KEYS = [resource]
+	local cache_key = redis.pcall('HMGET', KEYS[1], 'last_request_time', 'surplus_token')
+	local last_request_time = cache_key[1] -- 上次请求时间
+	local surplus_token = tonumber(cache_key[2]) -- 剩余的令牌数
+	local bucket_token = tonumber(ARGV[1]) -- 令牌桶最大数
+	local token_rate = tonumber(ARGV[2]) -- 令牌数生成速率/秒
+	local now_request_time = tonumber(ARGV[3]) -- 当前请求时间/毫秒
+	local token_ms_rate = token_rate/1000 -- 每毫秒生产令牌速率
+	local past_time = 0 -- 两次请求时间差
+	if surplus_token == nil then
+		surplus_token = bucket_token -- 填充剩余令牌数最大值
+		last_request_time = now_request_time -- 填充上次请求时间
 	else
-		-- 处于流量中
-		past_time = current_time - last_time --经过了多少时间
-	
+		past_time = now_request_time - last_request_time -- 填充两次请求时间差
 		if past_time <= 0 then
-			--高并发下每个服务的时间可能不一致
-			past_time = 0 -- 强制变成0 此处可能会出现少量误差
+			past_time = 0 -- 防止多台服务器出现时间差小于0
 		end
-		-- 两次请求期间内应该生成多少个token
-		local generated_nums = math.floor(past_time * rateOfperMills)  -- 向下取整，多余的认为还没生成完
-		stored_token_nums = math.min((stored_token_nums + generated_nums), max_token) -- 合并所有的令牌后不能超过设定的最大令牌数
+		local add_token = math.floor(past_time * token_ms_rate)  -- 通过时间差生成令牌数,向下取整
+		surplus_token = math.min((surplus_token + add_token), bucket_token) -- 剩余令牌数+生成令牌数 <= 令牌桶最大数
 	end
-	
-	local returnVal = 0 -- 返回值
-	
-	if stored_token_nums > 0 then
-		returnVal = 1 -- 通过限流
-		stored_token_nums = stored_token_nums - 1 -- 减少令牌
-		-- 必须要在获得令牌后才能重新记录时间。举例: 当每隔2ms请求一次时,只要第一次没有获取到token,那么后续会无法生产token,永远只过去了2ms
-		last_time = last_time + past_time
+	local status = 0 -- 判定状态 0.拒绝 1.通过
+	if surplus_token > 0 then
+		surplus_token = surplus_token - 1 -- 通过则剩余令牌数-1
+		last_request_time = last_request_time + past_time -- 刷新最后请求时间
+		redis.call('HMSET', KEYS[1], 'last_request_time', last_request_time, 'surplus_token', surplus_token) -- 更新剩余令牌数和最后请求时间
+		status = 1
 	end
-	
-	-- 更新缓存
-	redis.call('HMSET', KEYS[1], 'last_time', last_time, 'stored_token_nums', stored_token_nums)
-	-- 设置超时时间
-	-- 令牌桶满额的时间(超时时间)(ms) = 空缺的令牌数 * 生成一枚令牌所需要的毫秒数(1 / 每毫秒生产令牌速率)
-	redis.call('PEXPIRE', KEYS[1], math.ceil((1/rateOfperMills) * (max_token - stored_token_nums)))
-	
-	return returnVal
+	if surplus_token < 0 then
+		redis.call('PEXPIRE', KEYS[1], 3000) -- 设置超时重置数据
+	end
+	return status
 `)
 
 type RedisRateLimiter struct {
