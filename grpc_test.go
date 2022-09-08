7645c50dfda8caceb8c58a36e01a095e6176aa32
godaddy-x <shadowsick@sina.com>
2022-09-08 14:59:01 +0800

    fix test

--- grpc_test.go
+++ grpc_test.go
@@ -6,7 +6,12 @@ import (
 	"github.com/godaddy-x/freego/consul/grpcx"
 	"github.com/godaddy-x/freego/consul/grpcx/impl"
 	"github.com/godaddy-x/freego/consul/grpcx/pb"
+	"github.com/shimingyah/pool"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/metadata"
+	"net"
 	"testing"
 )
 
@@ -34,4 +39,73 @@ func TestConsulxCallGRPC_GenID(t *testing.T) {
 	}
 	object, _ := res.(*pb.GenerateIdRes)
 	fmt.Println("call rpc:", object)
+}
+
+func TestGRPCServer(t *testing.T) {
+	l, err := net.Listen("tcp", "127.0.0.1:8889")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("listen on 127.0.0.1:8889")
+	opts := []grpc.ServerOption{
+		grpc.InitialWindowSize(pool.InitialWindowSize),
+		grpc.InitialConnWindowSize(pool.InitialConnWindowSize),
+		grpc.MaxSendMsgSize(pool.MaxSendMsgSize),
+		grpc.MaxRecvMsgSize(pool.MaxRecvMsgSize),
+		grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
+			PermitWithoutStream: true,
+		}),
+		grpc.KeepaliveParams(keepalive.ServerParameters{
+			Time:    pool.KeepAliveTime,
+			Timeout: pool.KeepAliveTimeout,
+		}),
+	}
+	grpcServer := grpc.NewServer(opts...)
+
+	pb.RegisterPubWorkerServer(grpcServer, &impl.PubWorker{})
+
+	err = grpcServer.Serve(l)
+
+	if err != nil {
+		println(err)
+	}
+}
+
+var conn *grpc.ClientConn
+
+func init() {
+	conn, _ = grpc.Dial("localhost:8889", grpc.WithTransportCredentials(insecure.NewCredentials()))
+}
+
+const accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlMTBhZGMzOTQ5YmE1OWFiYmU1NmUwNTdmMjBmODgzZSIsImF1ZCI6IiIsImlzcyI6IiIsImlhdCI6MCwiZXhwIjoxNjYyNjIxMTk0LCJkZXYiOiJHUlBDIiwianRpIjoiaTRkS1FaQ25zejJZd0JSbVFkUHRnUT09IiwiZXh0Ijp7fX0=.wqWOml4WecF2f5U1eaxzv/wYmIwFG5JZGk6nVW/sWIQ="
+
+func TestGRPCClient(t *testing.T) {
+	fmt.Println(conn.Target())
+	md := metadata.New(map[string]string{"token": accessToken})
+	ctx := context.Background()
+	ctx = metadata.NewOutgoingContext(ctx, md)
+	client := pb.NewPubWorkerClient(conn)
+	response, err := client.GenerateId(ctx, &pb.GenerateIdReq{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(response)
+}
+
+func BenchmarkGRPCClient(b *testing.B) {
+	b.StopTimer()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ { //use b.N for looping
+		ctx := context.Background()
+		//md := metadata.New(map[string]string{"token": accessToken})
+		//ctx = metadata.NewOutgoingContext(ctx, md)
+		client := pb.NewPubWorkerClient(conn)
+		_, err := client.GenerateId(ctx, &pb.GenerateIdReq{})
+		if err != nil {
+			panic(err)
+		}
+		//fmt.Println(r)
+	}
 }
