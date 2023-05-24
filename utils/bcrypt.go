65cdb83226ebf0aa0eaf7e78138edace21545177
godaddy-x <shadowsick@sina.com>
2023-05-24 13:12:51 +0800

    fix bcrypt

--- utils/bcrypt.go
+++ utils/bcrypt.go
@@ -1,11 +1,20 @@
 package utils
 
-func randKey() string {
+func newKey() string {
 	return MD5(AddStr(GetUUID(), AnyToStr(UnixNano()), RandStr(32)))
 }
 
-func newHash(password, salt, randKey string) string {
-	return HMAC_SHA512(HMAC_SHA512(password, randKey), HMAC_SHA512(HMAC_SHA512(salt, randKey), HMAC_SHA512(GetLocalSecretKey(), randKey)))
+func newHash(password, salt, key string) string {
+	return HMAC_SHA256(HMAC_SHA512(password, key), HMAC_SHA512(HMAC_SHA512(salt, key), HMAC_SHA512(GetLocalSecretKey(), key)))
+}
+
+func recHash(hash, salt, key string, index int) string {
+	if index > 16 {
+		return hash
+	}
+	hash = HMAC_SHA256(hash, HMAC_SHA256(salt, key))
+	index++
+	return recHash(hash, salt, key, index)
 }
 
 func mergeHash(hash, key string) string {
@@ -13,16 +22,18 @@ func mergeHash(hash, key string) string {
 }
 
 func PasswordHash(password, salt string) string {
-	key := randKey()
+	key := newKey()
 	hash := newHash(password, salt, key)
+	hash = recHash(hash, salt, key, 0)
 	return mergeHash(hash, key)
 }
 
 func PasswordVerify(password, salt, target string) bool {
-	if len(target) != 160 {
+	if len(target) != 96 {
 		return false
 	}
-	key := target[128:]
+	key := target[64:]
 	hash := newHash(password, salt, key)
+	hash = recHash(hash, salt, key, 0)
 	return mergeHash(hash, key) == target
 }
